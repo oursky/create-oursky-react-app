5884fc805dde3da83c9ecf9526d6ec306d7ef185
Louis Chan <louischan@oursky.com>
2018-02-21 15:12:17 +0800

    Move setup to index.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
 import { PureComponent } from "react";
-import createStore from "./redux/createStore";
-import { Provider } from "react-redux";
 import styles from "./App.module.scss";
-
-const store = createStore();
 
 class App extends PureComponent {
   render() {
-    return (
-      <Provider store={store}>
-        <div className={styles.container} />
-      </Provider>
-    );
+    return <div className={styles.container} />;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
 // Include polyfill for any compiled javascript > ES5
 import "babel-polyfill";
+
 // Include polyfill for ECMA-402
 import "intl";
-
 // Include the locales that you need to support
 // You always need English because we use it as our development language
 import "intl/locale-data/jsonp/en.js";
-
 // Here are some examples
 // If you know the language and the region you need to support
 // then you should use the more specific locale data.
 // This is because the more specific the locale data,
 // the more precise the language convention is.
 // The full list can be found in "/node_modules/intl/locale-data/jsonp"
-
 // Japanese
-// import "intl/locale-data/jsonp/ja.js"
-
+// import "intl/locale-data/jsonp/ja.js";
 // Japanese used in Japan
-// import "intl/locale-data/jsonp/ja-JP.js"
-
+// import "intl/locale-data/jsonp/ja-JP.js";
 // Chinese
 // But please do NOT use it because
 // written Chinese must have an associated script (Hant or Hans)
-// import "intl/locale-data/jsonp/zh.js"
-
+// import "intl/locale-data/jsonp/zh.js";
 // Traditional Chinese
-// import "intl/locale-data/jsonp/zh-Hant.js"
-
+// import "intl/locale-data/jsonp/zh-Hant.js";
 // Traditional Chinese used in Hong Kong
 import "intl/locale-data/jsonp/zh-Hant-HK.js";
-
 // Traditional Chinese used in Taiwan
-// import "intl/locale-data/jsonp/zh-Hant-TW.js"
+// import "intl/locale-data/jsonp/zh-Hant-TW.js";
 
 import React from "react";
 import { render } from "react-dom";
+import { Provider } from "react-redux";
+import createStore from "./redux/createStore";
 import App from "./App";
 
-render(<App />, document.getElementById("root"));
+const store = createStore();
+const element = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+render(element, document.getElementById("root"));
