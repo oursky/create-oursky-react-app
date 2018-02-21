42077f316017a1977ee2c8829b9a182757260b85
Louis Chan <louischan@oursky.com>
2018-02-21 19:52:17 +0800

    Demostrate how to localize DOM attributes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,44 @@
 import * as React from "react";
 import { PureComponent } from "react";
 import styles from "./App.module.scss";
-import { FormattedMessage } from "./intl/components";
+import { FormattedMessage, Input } from "./intl/components";
 import ChangeLocaleButton from "./components/ChangeLocaleButton";
 
-class App extends PureComponent {
+type LocalState = {
+  inputValue: string;
+};
+
+class App extends PureComponent<{}, LocalState> {
+  input?: HTMLInputElement | null;
+
+  state = {
+    inputValue: "",
+  };
+
   onClick = (e: React.SyntheticEvent<HTMLButtonElement>) => {
     e.preventDefault();
     e.stopPropagation();
   };
 
+  onInputRef = (r: HTMLInputElement | null) => {
+    this.input = r;
+  };
+
+  onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({
+      inputValue: e.currentTarget.value,
+    });
+  };
+
   render() {
     return (
       <div className={styles.container}>
+        <Input
+          placeholderId="app.inputPlaceholder"
+          onChange={this.onChange}
+          onRef={this.onInputRef}
+          value={this.state.inputValue}
+        />
         <ChangeLocaleButton />
         <FormattedMessage id="app.hello" />
       </div>
--- src/intl/components.tsx
+++ src/intl/components.tsx
@@ -4,22 +4,43 @@ import * as PropTypes from "prop-types";
 import { connect } from "react-redux";
 import {
   InjectedIntl,
+  MessageValue,
   // You only need these 3 components
   FormattedMessage as FM,
   FormattedDate as FD,
   FormattedRelative as FR,
 } from "react-intl";
+// TODO: Replace Diff with Exclude (available from TypeScript >= 2.8)
+import { Diff } from "type-zoo";
 import { RootState } from "../redux/types";
 
-interface ReduxIntlContextOwnProps {
-  children: React.ReactNode;
+interface ForwardRef<T> {
+  // We cannot use "ref" as the prop name because
+  // "ref" is treated specially by react
+  onRef?: (instance: T | null) => void;
 }
-interface ReduxIntlContextConnectedProps {
+
+interface Values {
+  [key: string]: MessageValue;
+}
+
+interface OwnProps {
+  children: (intl: InjectedIntl) => React.ReactNode;
+}
+
+interface ConnectedProps {
   intl?: InjectedIntl;
 }
-type ReduxIntlContextProps = ReduxIntlContextOwnProps &
-  ReduxIntlContextConnectedProps;
-class ReduxIntlContext extends PureComponent<ReduxIntlContextProps> {
+
+type Props = OwnProps & ConnectedProps;
+
+// This class serves two use-cases.
+// 1. Extract the synchronized intl from redux store and provide it as context
+// 2. Render any children that needs intl to localize attributes
+//
+// Wrapping <Formatted*> from "react-intl" is the first use-case.
+// Wrapping DOM components is the second use-case.
+class ReduxIntlContext extends PureComponent<Props> {
   static childContextTypes = {
     intl: PropTypes.any,
   };
@@ -31,14 +52,15 @@ class ReduxIntlContext extends PureComponent<ReduxIntlContextProps> {
   }
 
   render() {
-    if (this.props.intl == null) {
+    const { intl, children } = this.props;
+    if (intl == null) {
       return null;
     }
-    return this.props.children;
+    return children(intl);
   }
 }
 
-function mapStateToProps(state: RootState): ReduxIntlContextConnectedProps {
+function mapStateToProps(state: RootState): ConnectedProps {
   return {
     intl: state.app.intl,
   };
@@ -46,25 +68,58 @@ function mapStateToProps(state: RootState): ReduxIntlContextConnectedProps {
 const C = connect(mapStateToProps)(ReduxIntlContext);
 
 export const FormattedMessage: React.SFC<FM.Props> = (props: FM.Props) => {
-  return (
-    <C>
-      <FM {...props} />
-    </C>
-  );
+  return <C>{() => <FM {...props} />}</C>;
 };
 
 export const FormattedDate: React.SFC<FD.Props> = (props: FD.Props) => {
-  return (
-    <C>
-      <FD {...props} />
-    </C>
-  );
+  return <C>{() => <FD {...props} />}</C>;
 };
 
 export const FormattedRelative: React.SFC<FR.Props> = (props: FR.Props) => {
-  return (
-    <C>
-      <FR {...props} />
-    </C>
-  );
+  return <C>{() => <FR {...props} />}</C>;
 };
+
+// Here is an example of how to localize intrinsic DOM component in a typesafe way
+// 1. Find out the original props (e.g. InputOriginalProps)
+// 2. Create a Props type that replaces the localizable attributes with <attr>Id and <attr>Values
+// 3. Wrap the DOM component and do localization imperatively
+// 4. Cast the component to SFC so that the user cannot mistakenly use "ref"
+// 5. Export the SFC
+type InputOriginalProps = React.InputHTMLAttributes<HTMLInputElement>;
+type InputProps = {
+  [P in Diff<keyof InputOriginalProps, "placeholder">]?: InputOriginalProps[P]
+} & {
+  placeholderId: string;
+  placeholderValues?: Values;
+} & ForwardRef<HTMLInputElement>;
+class Input_ extends PureComponent<InputProps> {
+  onRef = (instance: HTMLInputElement | null) => {
+    const { onRef } = this.props;
+    if (onRef != null) {
+      onRef(instance);
+    }
+  };
+
+  render() {
+    return (
+      <C>
+        {(intl: InjectedIntl) => {
+          const {
+            placeholderId,
+            placeholderValues,
+            onRef,
+            // We must ensure that rest contains only original props
+            ...rest
+          } = this.props;
+          // Imperatively localize necessary dom attributes
+          const placeholder = intl.formatMessage(
+            { id: placeholderId },
+            placeholderValues
+          );
+          return <input {...rest} ref={this.onRef} placeholder={placeholder} />;
+        }}
+      </C>
+    );
+  }
+}
+export const Input = (Input_ as any) as React.SFC<InputProps>;
